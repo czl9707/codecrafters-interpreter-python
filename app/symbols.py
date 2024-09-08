e9638f533287c38a5a3e8fd27c5caff12f27d4b0
zane <czl970721@gmail.com>
2024-09-08 10:20:04 -0400

    add more symbol

---
+++ app/symbols.py
@@ -0,0 +1,63 @@
+
+from abc import ABC
+from typing import Type
+
+
+class Symbol(ABC):
+    __slots__ = ["value"]
+    _chr2symbol: dict[str, Type['Symbol']] = {}
+    
+    symbol: str
+    name: str
+    
+    @classmethod
+    def __init_subclass__(cls: Type["Symbol"]) -> None:
+        Symbol._chr2symbol[cls.symbol] = cls
+    
+    @staticmethod
+    def is_symbol(symbol: str) -> bool:
+        return symbol in Symbol._chr2symbol
+    
+    @classmethod
+    def from_chr(cls, ch: str) -> "Symbol":
+        return Symbol._chr2symbol[ch]()
+    
+    def __str__(self) -> str:
+        return f"{self.name} {self.symbol} null"
+
+
+class LeftBraceSymbol(Symbol):
+    symbol = "{"
+    name = "LEFT_BRACE"
+    
+class RightBraceSymbol(Symbol):
+    symbol = "}"
+    name = "RIGHT_BRACE"
+    
+class LeftParenthesisSymbol(Symbol):
+    symbol = "("
+    name = "LEFT_PAREN"
+    
+class RightParenthesisSymbol(Symbol):
+    symbol = ")"
+    name = "RIGHT_PAREN"
+    
+class StarSymbol(Symbol):
+    symbol= "*" 
+    name="STAR"
+    
+class DotSymbol(Symbol):
+    symbol= "." 
+    name="DOT"
+    
+class CommaSymbol(Symbol):
+    symbol= "," 
+    name="COMMA"
+    
+class PlusSymbol(Symbol):
+    symbol= "+" 
+    name="PLUS"
+    
+class EOFSymbol(Symbol):
+    symbol = ""
+    name = "EOF"
--- app/tokenize.py
+++ app/tokenize.py
@@ -1,60 +1,7 @@
-from abc import ABC
 from argparse import ArgumentParser, Namespace
-from typing import Type
 
+from .symbols import Symbol, EOFSymbol
 
-SYMBOLS = {
-    "{":"LEFT_BRACE",
-    "}":"RIGHT_BRACE",
-    "(":"LEFT_PAREN",
-    ")":"RIGHT_PAREN",
-}
-
-class Symbol(ABC):
-    __slots__ = ["value"]
-    _chr2symbol: dict[str, Type['Symbol']] = {}
-    
-    symbol: str
-    name: str
-    
-    @classmethod
-    def __init_subclass__(cls: Type["Symbol"]) -> None:
-        Symbol._chr2symbol[cls.symbol] = cls
-    
-    @staticmethod
-    def is_symbol(symbol: str) -> bool:
-        return symbol in Symbol._chr2symbol
-    
-    @classmethod
-    def from_chr(cls, ch: str) -> "Symbol":
-        return Symbol._chr2symbol[ch]()
-    
-    def __str__(self) -> str:
-        return f"{self.name} {self.symbol} null"
-
-class LeftBraceSymbol(Symbol):
-    symbol = "{"
-    name = "LEFT_BRACE"
-    
-    
-class RightBraceSymbol(Symbol):
-    symbol = "}"
-    name = "RIGHT_BRACE"
-    
-    
-class LeftParenthesisSymbol(Symbol):
-    symbol = "("
-    name = "LEFT_PAREN"
-    
-    
-class RightParenthesisSymbol(Symbol):
-    symbol = ")"
-    name = "RIGHT_PAREN"
-    
-    
-class EOFSymbol(Symbol):
-    symbol = ""
-    name = "EOF"
     
 
 def config_tokenize_parser(arg_parser: ArgumentParser) -> None:
