b6f2ebf426ef193b6b3b154a30512cc115067cd1
zane <czl970721@gmail.com>
2024-09-15 16:27:40 -0400

    fix binary tree

--- app/expressions/expressions.py
+++ app/expressions/expressions.py
@@ -254,9 +254,8 @@ class BinaryExpression(Expression, ABC):
         right_node = cls.__insert_self_node(token, current.right, iter)
         if current.__class__ == right_node.__class__ and not current._right_associative:
             assert isinstance(right_node, BinaryExpression)
-            temp = current.right
             current.right = right_node.left
-            right_node.left = temp
+            right_node.left = current
                         
             return right_node
         else:
